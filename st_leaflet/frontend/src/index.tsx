d5b4426247f379842050c6e393afdabc8ed9b91a
Qiusheng Wu <giswqs@gmail.com>
2022-10-11 10:55:50 -0400

    Initial commit

---
+++ st_leaflet/frontend/src/index.tsx
@@ -0,0 +1,10 @@
+import React from "react"
+import ReactDOM from "react-dom"
+import LeafletComponent from "./LeafletComponent"
+
+ReactDOM.render(
+  <React.StrictMode>
+    <LeafletComponent />
+  </React.StrictMode>,
+  document.getElementById("root")
+)
